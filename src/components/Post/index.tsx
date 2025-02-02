96b05413aaeb7c4a9a58ebbf80737c56f2e0cf48
gskaeru <98203312+gskaeru@users.noreply.github.com>
2025-02-02 14:34:50 +0900

    Update：day3

---
+++ src/components/Post/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./style.module.scss";
+import { PostProps } from "@/pages";
+
+const Post: React.FC<PostProps> = ({ id, name, date, content }) => {
+  // const Post = ({ name, date, content }: PostProps) => {
+  return (
+    <div className={styles.post} key={id}>
+      <p>{id}</p>
+      <p>{name}</p>
+      <p>{date}</p>
+      <p>{content}</p>
+    </div>
+  );
+};
+
+export default Post;
